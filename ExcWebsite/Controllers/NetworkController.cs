5dcc84fd792b4e74f49d51930470817978bb7754
Leen Aboukhalil <109052421+Leen-ak@users.noreply.github.com>
2025-02-04 16:13:31 -0500

    GetAll UserName is working for swagger

---
+++ ExcWebsite/Controllers/NetworkController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ViewModels;
+using DAL;
+using System.Linq;
+
+namespace ExcWebsite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetworkController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        {
+            try
+            {
+                NetworkVM vm = new();
+                List<NetworkVM> allUsers = await vm.GetAll();
+                return Ok(allUsers);
+
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine("Problem in " + GetType().Name + " " +
+                MethodBase.GetCurrentMethod()!.Name + " " + ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
